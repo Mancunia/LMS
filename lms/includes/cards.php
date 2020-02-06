b5654940d8d5e36194585d39e6eae1b4e861a9a2
Emmanuel Osei Mensah <mancuniamoe@gmail.com>
2020-02-06 11:45:52 +0000

    still cleaning

---
+++ lms/includes/cards.php
@@ -0,0 +1,55 @@
+ <!-- Icon Cards-->
+ <div class="row ">
+          <div class="col-xl-4 col-sm-6 mb-4">
+            <div class="card text-white bg-primary o-hidden h-100">
+              <div class="card-body">
+                <div class="card-body-icon">
+                  <i class="fas fa-fw fa-arrow-down"></i>
+                </div>
+                <div class="mr-5"><h1>26 </h1> <h5>Received!</h5> </div>
+              </div>
+              <a class="card-footer text-white clearfix small z-1" href="#">
+                <span class="float-left">View Details</span>
+                <span class="float-right">
+                  <i class="fas fa-angle-right"></i>
+                </span>
+              </a>
+            </div>
+          </div>
+
+          <div class="col-xl-4 col-sm-6 mb-4">
+            <div class="card text-white bg-warning o-hidden h-100">
+              <div class="card-body">
+                <div class="card-body-icon">
+                  <i class="fas fa-fw fa-history"></i>
+                </div>
+                <div class="mr-5"><h1>11 </h1> <h5>Dispatching!</h5></div>
+              </div>
+              <a class="card-footer text-white clearfix small z-1" href="#">
+                <span class="float-left">View Details</span>
+                <span class="float-right">
+                  <i class="fas fa-angle-right"></i>
+                </span>
+              </a>
+            </div>
+          </div>
+
+          <div class="col-xl-4 col-sm-6 mb-4">
+            <div class="card text-white bg-success o-hidden h-100">
+              <div class="card-body">
+                <div class="card-body-icon">
+                  <i class="fas fa-fw fa-arrow-up"></i>
+                </div>
+                <div class="mr-5"><h1>126 </h1> <h5>Dispatched!</h5></div>
+              </div>
+              <a class="card-footer text-white clearfix small z-1" href="#">
+                <span class="float-left">View Details</span>
+                <span class="float-right">
+                  <i class="fas fa-angle-right"></i>
+                </span>
+              </a>
+            </div>
+          </div>
+
+         
+        </div>
---
+++ lms/index.php
@@ -0,0 +1,424 @@
+<?php
+
+
+
+include_once 'requires/header.php';
+
+?>
+
+
+
+  
+
+   <?php
+
+   include_once 'requires/nav_bar.php';
+
+   ?>
+
+  
+
+   <?php
+
+   include_once 'requires/sidebar.php';
+
+
+   ?>
+
+    <div id="content-wrapper">
+
+      <div class="container-fluid">
+
+        <!-- Breadcrumbs-->
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item">
+            <a href="#">Dashboard</a>
+          </li>
+          <li class="breadcrumb-item active">Overview</li>
+        </ol>
+
+       <?php
+       include_once 'includes/cards.php';
+       ?>
+
+        <!-- DataTables Example -->
+        <!-- <div class="card mb-3">
+          <div class="card-header">
+            <i class="fas fa-table"></i>
+            Data Table Example</div>
+          <div class="card-body">
+            <div class="table-responsive">
+              <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                <thead>
+                  <tr>
+                    <th>S/N</th>
+                    <th>Ref.No</th>
+                    <th>Subjext</th>
+                    <th>From</th>
+                    <th>Date</th>
+                    
+                  </tr>
+                </thead>
+                <tfoot>
+                  <tr>
+                  <th>S/N</th>
+                    <th>Ref.No</th>
+                    <th>Subjext</th>
+                    <th>From</th>
+                    <th>Date</th>
+                  </tr>
+                </tfoot>
+                <tbody>
+                  <tr>
+                    <td>61</td>
+                    <td>Tiger Nixon</td>
+                    <td>System Architect</td>
+                    <td>Edinburgh</td>
+                    <td>2011/04/25</td>
+                  </tr>
+                 
+                </tbody>
+              </table>
+            </div>
+          </div>
+          <div class="card-footer small text-muted">Updated yesterday at 11:59 PM</div>
+        </div>
+      </div>  -->
+
+
+      <!-- .................................................................................... -->
+
+      <div class="card mb-3">
+          <div class="card-header bg-primary" id="received">
+            <i class="fas fa-arrow-down"></i>
+            Received</div>
+          <div class="card-body">
+            <div class="table-responsive">
+      <table id="" class="display table table-hover" style="width:100%">
+        <thead>
+        <tr>
+                    <th>S/N</th>
+                    <th>Ref.No</th>
+                    <th>Subject</th>
+                    <th>From</th>
+                    <th>Date</th>
+                    
+                  </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Tiger Nixon</td>
+                <td>System Architect</td>
+                <td>Edinburgh</td>
+                <td>61</td>
+                <td>$320,800</td>
+            </tr>
+            <tr>
+                <td>Cedric Kelly</td>
+                <td>Senior Javascript Developer</td>
+                <td>Edinburgh</td>
+                <td>22</td>
+                <td>$433,060</td>
+            </tr>
+            <tr>
+                <td>Sonya Frost</td>
+                <td>Software Engineer</td>
+                <td>Edinburgh</td>
+                <td>23</td>
+                <td>$103,600</td>
+            </tr>
+            <tr>
+                <td>Quinn Flynn</td>
+                <td>Support Lead</td>
+                <td>Edinburgh</td>
+                <td>22</td>
+                <td>$342,000</td>
+            </tr>
+            <tr>
+                <td>Dai Rios</td>
+                <td>Personnel Lead</td>
+                <td>Edinburgh</td>
+                <td>35</td>
+                <td>$217,500</td>
+            </tr>
+            <tr>
+                <td>Gavin Joyce</td>
+                <td>Developer</td>
+                <td>Edinburgh</td>
+                <td>42</td>
+                <td>$92,575</td>
+            </tr>
+            <tr>
+                <td>Martena Mccray</td>
+                <td>Post-Sales support</td>
+                <td>Edinburgh</td>
+                <td>46</td>
+                <td>$324,050</td>
+            </tr>
+            <tr>
+                <td>Jennifer Acosta</td>
+                <td>Junior Javascript Developer</td>
+                <td>Edinburgh</td>
+                <td>43</td>
+                <td>$75,650</td>
+            </tr>
+            <tr>
+                <td>Shad Decker</td>
+                <td>Regional Director</td>
+                <td>Edinburgh</td>
+                <td>51</td>
+                <td>$183,000</td>
+            </tr>
+        </tbody>
+        <tfoot>
+        <tr>
+                    <th>S/N</th>
+                    <th>Ref.No</th>
+                    <th>Subject</th>
+                    <th>From</th>
+                    <th>Date</th>
+                    
+                  </tr>
+        </tfoot>
+    </table>
+</div>
+</div>
+</div>
+
+<hr>
+
+
+    <!-- .................................................................................... -->
+    
+    <div class="card mb-3">
+          <div class="card-header bg-warning" id="dispatching">
+            <i class="fas fa-history"></i>
+            Dispatching</div>
+          <div class="card-body">
+            <div class="table-responsive">
+    <table id="" class="display table table-hover" style="width:100%">
+        <thead>
+        <tr>
+                    <th>S/N</th>
+                    <th>Ref.No</th>
+                    <th>Subject</th>
+                    <th>From</th>
+                    <th>Date</th>
+                    
+                  </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Jena Gaines</td>
+                <td>Office Manager</td>
+                <td>London</td>
+                <td>30</td>
+                <td>$90,560</td>
+            </tr>
+            <tr>
+                <td>Haley Kennedy</td>
+                <td>Senior Marketing Designer</td>
+                <td>London</td>
+                <td>43</td>
+                <td>$313,500</td>
+            </tr>
+            <tr>
+                <td>Tatyana Fitzpatrick</td>
+                <td>Regional Director</td>
+                <td>London</td>
+                <td>19</td>
+                <td>$385,750</td>
+            </tr>
+            <tr>
+                <td>Michael Silva</td>
+                <td>Marketing Designer</td>
+                <td>London</td>
+                <td>66</td>
+                <td>$198,500</td>
+            </tr>
+            <tr>
+                <td>Bradley Greer</td>
+                <td>Software Engineer</td>
+                <td>London</td>
+                <td>41</td>
+                <td>$132,000</td>
+            </tr>
+            <tr>
+                <td>Angelica Ramos</td>
+                <td>Chief Executive Officer (CEO)</td>
+                <td>London</td>
+                <td>47</td>
+                <td>$1,200,000</td>
+            </tr>
+            <tr>
+                <td>Suki Burks</td>
+                <td>Developer</td>
+                <td>London</td>
+                <td>53</td>
+                <td>$114,500</td>
+            </tr>
+            <tr>
+                <td>Prescott Bartlett</td>
+                <td>Technical Author</td>
+                <td>London</td>
+                <td>27</td>
+                <td>$145,000</td>
+            </tr>
+            <tr>
+                <td>Timothy Mooney</td>
+                <td>Office Manager</td>
+                <td>London</td>
+                <td>37</td>
+                <td>$136,200</td>
+            </tr>
+            <tr>
+                <td>Bruno Nash</td>
+                <td>Software Engineer</td>
+                <td>London</td>
+                <td>38</td>
+                <td>$163,500</td>
+            </tr>
+            <tr>
+                <td>Hermione Butler</td>
+                <td>Regional Director</td>
+                <td>London</td>
+                <td>47</td>
+                <td>$356,250</td>
+            </tr>
+            <tr>
+                <td>Lael Greer</td>
+                <td>Systems Administrator</td>
+                <td>London</td>
+                <td>21</td>
+                <td>$103,500</td>
+            </tr>
+        </tbody>
+        <tfoot>
+        <tr>
+                    <th>S/N</th>
+                    <th>Ref.No</th>
+                    <th>Subject</th>
+                    <th>From</th>
+                    <th>Date</th>
+                    
+                  </tr>
+        </tfoot>
+    </table>
+    </div>
+</div>
+</div>
+
+
+<hr>
+
+<div class="card mb-3">
+          <div class="card-header bg-success" id="dispatched">
+            <i class="fas fa-arrow-up"></i>
+            Data Table Example</div>
+          <div class="card-body">
+            <div class="table-responsive">
+      <table id="" class="display table table-hover" style="width:100%">
+        <thead>
+        <tr>
+                    <th>S/N</th>
+                    <th>Ref.No</th>
+                    <th>Subject</th>
+                    <th>From</th>
+                    <th>Date</th>
+                    
+                  </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Tiger Nixon</td>
+                <td>System Architect</td>
+                <td>Edinburgh</td>
+                <td>61</td>
+                <td>$320,800</td>
+            </tr>
+            <tr>
+                <td>Cedric Kelly</td>
+                <td>Senior Javascript Developer</td>
+                <td>Edinburgh</td>
+                <td>22</td>
+                <td>$433,060</td>
+            </tr>
+            <tr>
+                <td>Sonya Frost</td>
+                <td>Software Engineer</td>
+                <td>Edinburgh</td>
+                <td>23</td>
+                <td>$103,600</td>
+            </tr>
+            <tr>
+                <td>Quinn Flynn</td>
+                <td>Support Lead</td>
+                <td>Edinburgh</td>
+                <td>22</td>
+                <td>$342,000</td>
+            </tr>
+            <tr>
+                <td>Dai Rios</td>
+                <td>Personnel Lead</td>
+                <td>Edinburgh</td>
+                <td>35</td>
+                <td>$217,500</td>
+            </tr>
+            <tr>
+                <td>Gavin Joyce</td>
+                <td>Developer</td>
+                <td>Edinburgh</td>
+                <td>42</td>
+                <td>$92,575</td>
+            </tr>
+            <tr>
+                <td>Martena Mccray</td>
+                <td>Post-Sales support</td>
+                <td>Edinburgh</td>
+                <td>46</td>
+                <td>$324,050</td>
+            </tr>
+            <tr>
+                <td>Jennifer Acosta</td>
+                <td>Junior Javascript Developer</td>
+                <td>Edinburgh</td>
+                <td>43</td>
+                <td>$75,650</td>
+            </tr>
+            <tr>
+                <td>Shad Decker</td>
+                <td>Regional Director</td>
+                <td>Edinburgh</td>
+                <td>51</td>
+                <td>$183,000</td>
+            </tr>
+        </tbody>
+        <tfoot>
+        <tr>
+                    <th>S/N</th>
+                    <th>Ref.No</th>
+                    <th>Subject</th>
+                    <th>From</th>
+                    <th>Date</th>
+                    
+                  </tr>
+        </tfoot>
+    </table>
+</div>
+</div>
+</div>
+
+<hr>
+
+
+      
+
+
+      <!-- /.container-fluid -->
+
+      <!-- Sticky Footer -->
+     <?php
+     require_once 'requires/footer.php';
+     ?>
+
+
